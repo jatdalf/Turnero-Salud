4ceef228781c2232ad8f3800757359c7b6869d98
jatdalf <70412644+jatdalf@users.noreply.github.com>
2024-02-13 11:26:02 -0300

    responsive

--- src/views/Agendar/agendar.tsx
+++ src/views/Agendar/agendar.tsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom"
 import styles from './agendar.module.css'
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const Agendar = ()=>{
+    const [value, onChange] = useState<Value>(new Date());
+
     return(
         <>
           <h2 className={styles.agendarH2}>Agendar nuevo turno</h2>
           <form>
             <fieldset className={styles.agendarFieldset}>
                 <legend>Ingrese los datos</legend>
-                <p>Fecha:</p>
+                <p>Fecha:</p><Calendar onChange={onChange} value={value} />
                 <p>Rango horario:</p>
                 <p>Proveedor:</p>
                 <p>Orden de provisión:</p>
