d56299a8716ebe92dbfe2512245bb8bf2625342b
jatdalf <70412644+jatdalf@users.noreply.github.com>
2024-02-14 12:36:02 -0300

    agenda

--- src/views/Agendar/agendar.tsx
+++ src/views/Agendar/agendar.tsx
@@ -1,63 +1,151 @@
-import { Link } from "react-router-dom"
-import styles from './agendar.module.css'
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import { Link } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import guardar from '../../assets/guardar.png';
+import salir from '../../assets/salir.png';
+import styles from './agendar.module.css';
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-const Agendar: React.FC = ()=>{
-    const [value, onChange] = useState<Value>(new Date());
-        // Paso 2: Crear estado para la fecha actual
-    const [currentDate, setCurrentDate] = useState<string>(new Date().toISOString().slice(0, 10));
-    let fechaprimaria = currentDate;
-    
-    
-    // Manejar cambios en el input
-    const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setCurrentDate(event.target.value);
-    };
+const Agendar: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
 
-    return(
-        <>
-          <h2 className={styles.agendarH2}>Agendar nuevo turno</h2>
-          <form>
-            <fieldset className={styles.agendarFieldset}>
-              <legend>Ingrese los datos</legend>                 
-              <div >
-                <ul className={styles.contenedorAgenda}>
-                  <li>Fecha:</li>
-                  <li> 
-                    <input
-                        type="date"
-                        value={currentDate} // Asignar la fecha actual como valor
-                        onChange={handleDateChange} // Manejar cambios en el input
-                    />
-                  </li>
-                  {/* <li><Calendar onChange={onChange} value={value} /></li>                   */}
-                </ul>
-                  <p>Rango horario:</p>
-                  <p>Proveedor:</p>
-                  <p>Orden de provisión:</p>
-                  <p>Volumen aproximado:</p>
-                  <p>Notas/Observaciones:</p>              
-              </div>
-                <hr />
-                <p>
-                    <fieldset>
-                      <legend>Turnos del dia</legend>
-                    </fieldset>
-                    <input type="submit" value="Guardar datos"></input>   
-                    <Link to="/Home">     
-                        <button>Salir</button>
-                    </Link>           
-                </p>
-            </fieldset>
-            </form>
-        </>
-    )
-}
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
+  };
 
+  const horaInicio: string = '09:00';
+  const intervalo: string = '00:30';
+  const horaFinal: string = '14:00';
+  const proveedores: string[] =[
+    "DNM",
+    "PROPATO",
+    "UNC",
+    "NACION",
+    "IDM",
+    "DEL LIBERTADOR",
+    "BIOQUIMICOS DE CBA",
+    "ERCO",
+    "SALUD RENAL",
+    "MEDICAL PRO",
+    "PHARMA CENTER",
+  ]
 
-export default Agendar
+  const sumarIntervalo = (hora: string, intervalo: string): string => {
+    const [horas, minutos] = hora.split(':').map(Number);
+    const [intervaloHoras, intervaloMinutos] = intervalo.split(':').map(Number);
+
+    let nuevaHora = horas + intervaloHoras;
+    let nuevosMinutos = minutos + intervaloMinutos;
+
+    if (nuevosMinutos >= 60) {
+      nuevaHora += 1;
+      nuevosMinutos -= 60;
+    }
+
+    return `${String(nuevaHora).padStart(2, '0')}:${String(nuevosMinutos).padStart(2, '0')}`;
+  };
+
+  const Horarios: string[] = [];
+  let horaActual: string = horaInicio;
+  let rangosHorarios: string[] = []
+
+  while (horaActual <= horaFinal) {
+    Horarios.push(horaActual);    
+    horaActual = sumarIntervalo(horaActual, intervalo);
+  }
+  for (let i = 1; i < Horarios.length; i++) {
+    rangosHorarios.push(Horarios[i-1] + " a " + Horarios[i])    
+  }
+ 
+  return (
+    <>
+      <h2 className={styles.agendarH2}>Agendar nuevo turno</h2>
+      <form>
+        <fieldset className={styles.agendarFieldset}>
+          <legend>Ingrese los datos</legend>
+          <div>
+            <ul className={styles.contenedorAgenda}>
+              <li>Fecha:</li>
+              <li>
+                <DatePicker
+                  className={styles.datepicker}
+                  selected={selectedDate}
+                  onChange={handleDateChange}
+                  dateFormat="dd/MM/yyyy"
+                />
+              </li>              
+            </ul>
+            <ul className={styles.contenedorAgenda}>
+              <li>Rango horario:</li>
+              <li>
+                <select className={styles.horaInicio} id="horaInicio">
+                  {rangosHorarios.map((hora, index) => (
+                    <option key={index} value={hora}>
+                      {hora}
+                    </option>
+                  ))}
+                </select>
+              </li>            
+            </ul>
+            <ul className={styles.contenedorAgenda}>
+              <li>Proveedor:</li>
+              <li>
+                <select className={styles.proveedores} id="proveedor">
+                  {proveedores.map((proveedor, index) => (
+                    <option key={index} value={proveedor}>
+                      {proveedor}
+                    </option>
+                  ))}
+                </select>
+              </li>            
+            </ul> <ul className={styles.contenedorAgenda}>
+              <li>Orden de provisión:</li>
+              <li>
+                <input className={styles.ordenProvision} id="op" type="text" placeholder='Ingrese OP/OC' required/>
+              </li>                       
+            </ul>
+            <ul className={styles.contenedorAgenda}>
+              <li>Volumen aproximado:</li>
+              <li>
+                <input className={styles.volumen} id="volumen" type="text" placeholder='Cant. pallets/Cajas' required/>
+              </li>                         
+            </ul>
+            <ul className={styles.contenedorAgenda}>
+              <li>Notas/Observaciones:</li>
+              <li>
+                <textarea className={styles.obs} id="obs" name="obs"  placeholder='Informacion adicional' />
+              </li>            
+            </ul>            
+          </div>
+          <hr />
+          <ul className={styles.contenedorAgenda}>
+            <li>
+              <fieldset className={styles.Turnos}>            
+                <legend>Turnos del dia</legend>
+              </fieldset>
+            </li>
+            <li>
+              <button className={styles.HeaderButton}>
+                <img className={styles.HeaderIcon} src={guardar} alt="salir/volver" />
+                <p className={styles.HeaderButton1Text}>Guardar Datos</p>
+              </button>
+              {/* </li>  
+                <li> */}
+            <Link to="/Home">
+              <button className={styles.HeaderButton}>
+                <img className={styles.HeaderIcon} src={salir} alt="salir/volver" />
+                <p className={styles.HeaderButton2Text}>Salir</p>
+              </button>
+            </Link>   
+            </li>       
+          </ul>
+        </fieldset>
+      </form>
+    </>
+  );
+};
+
+export default Agendar;
